53ccda384b369f56bb02145c7ab0cf6cf665f52c
milon-hossain <abireshan32@gmial.com>
2024-08-17 17:06:55 +0600

    add search

--- index.js
+++ index.js
@@ -19,10 +19,39 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
-
+ 
+const phoneCollection=client.db('Job_Task').collection('mobile')
 async function run() {
   try {
+
+    app.get('/phone', async (req, res) => {
+      try {
+        const page = parseInt(req.query.page) || 1;  
+        const limit = parseInt(req.query.limit) || 10;  
+        const search = req.query.search || '';
+        const skip = (page - 1) * limit;  
     
+        const searchFilter = search ? { productName: { $regex: search, $options: 'i' } } :{};
+
+        const result = await phoneCollection
+          .find(searchFilter)
+          .skip(skip)  
+          .limit(limit)  
+          .toArray();
+        const totalItems = await phoneCollection.countDocuments();
+        const totalPages = Math.ceil(totalItems / limit);
+    
+        
+        res.send({
+          items: result,  
+          totalPages,  
+          currentPage: page,  
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
+      }
+    });
     await client.connect();
     
     await client.db("admin").command({ ping: 1 });
